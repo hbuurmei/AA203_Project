778b815c18e0ac71e07c4b1cd7163390657b98b9
Hugo Buurmeijer <hugo.buurmeijer@gmail.com>
2023-05-26 22:54:13 -0700

    Remove vis file and test render for multiple agents

--- experiments.py
+++ experiments.py
@@ -5,7 +5,7 @@ from dqn import DQNAgent
 
 # Create an environment
 width, height = 10, 10
-N_agents, N_sats = 1, 0
+N_agents, N_sats = 3, 0
 action_range = 5
 p_move = 1  # weight of penalty of moving w.r.t. penalty of incorrect distribution estimate
 max_temp = 400  # maximum temperature of the fire, i.e. at the mean
@@ -15,3 +15,4 @@ init_sigma = np.array([[width/2, 0], [0, height/2]])
 init_state = np.vstack((init_positions, init_mu, init_sigma))
 
 env = WildFireEnv(width, height, init_state, action_range, p_move, max_temp, N_agents, N_sats)
+env.render()
