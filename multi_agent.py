65072d1fc4ac96ddacf03de1442061cc11e724f0
Hugo Buurmeijer <hugo.buurmeijer@gmail.com>
2023-06-05 23:19:52 -0700

    MA

--- multi_agent.py
+++ multi_agent.py
@@ -1,8 +1,7 @@
 import torch
 import argparse
 import numpy as np
-# from environment import WildFireEnv
-from env_weighted_old import WildFireEnv
+from env_weighted import WildFireEnv
 from dqn import DQNAgent
 import imageio
 import utils as ut
@@ -12,10 +11,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--num_agents', type=int, default=1, help='Number of agents')
 parser.add_argument('--num_sats', type=int, default=0, help='Number of satellites')
 parser.add_argument('--action_range', type=int, default=5, help='Action range')
-parser.add_argument('--p_move', type=float, default=0., help='Movement cost')
+parser.add_argument('--p_move', type=float, default=0.05, help='Movement cost')
 parser.add_argument('--env_size', type=int, default=10, help='Size of the environment')
 parser.add_argument('--rand_reset', type=bool, default=False, help='Randomly reset agent positions during training')
-parser.add_argument('--episode_count', type=int, default=5000, help='Number of episodes to train for')
+parser.add_argument('--episode_count', type=int, default=10000, help='Number of episodes to train for')
 
 args = parser.parse_args()
 
@@ -50,39 +49,39 @@ state_dim = env.flatten_state(init_state).shape[0]
 action_dim = (env.action_range**2)**N_agents
 agent = DQNAgent(env, state_dim, action_dim)
 
-TRAIN = False
+TRAIN = True
 if TRAIN:
     agent.train(num_episodes=args.episode_count)
-    # fig.savefig('./renderings/training_single_weight_rewards_plt_30x30.png')
-    # fig.savefig('./renderings/training_test_2agents_0p.png')
     # Save the model
-    torch.save(agent.model.state_dict(), 'models/dqn_training_test_1agents_0p_1000.pt')
+    torch.save(agent.model.state_dict(), 'models/dqn_training_test_2agents_10000_default.pt')
 
-# Test DQN
-agent.model.load_state_dict(torch.load('models/dqn_training_test_1agents_0p_1000.pt'))
-env.reset()
-# env.reposition(np.array([[1, 9], [2, 8], [3, 7]]))
-# env.reposition(np.array([[10, 3], [15, 8]]))
-# env.reposition(np.array([[8, 3]]))
+SIM = False
+if SIM:
+    # Test DQN
+    agent.model.load_state_dict(torch.load('models/dqn_training_test_2agents_10000_default.pt'))
+    env.reset()
+    # env.reposition(np.array([[1, 9], [2, 8], [3, 7]]))
+    # env.reposition(np.array([[9, 3], [2, 7]]))
+    # env.reposition(np.array([[8, 3]]))
 
-while not env.done:
-    print("Simulation step: ", env.step_count)
-    action = agent.act(env.flatten_state(env.state))
-    env.print_state()
-    env.step(action)
-    env.render()
+    while not env.done:
+        print("Simulation step: ", env.step_count)
+        action = agent.act(env.flatten_state(env.state))
+        env.print_state()
+        env.step(action)
+        env.render()
 
-# fig, ax = ut.plotKL(env)
-# fig.savefig('./renderings/KL_1agents_0p.png')
+    fig, ax = ut.plotKL(env)
+    fig.savefig('./renderings/KL_2agents_10000_default.png')
 
-# Render final state
-GIF = False
-if GIF:
-    frames = []
-    for t in range(1,env.step_count+1):
-        image = imageio.v2.imread(f'./renderings/step_{t}.png')
-        frames.append(image)
-    imageio.mimsave('./renderings/distributions_test_1agents_0p_5000.gif', # output gif
-                frames,          # array of input frames
-                duration = 500,         # optional: frames per second
-                loop = 1)        # optional: loop enabled - 1 for True; 0 for False
+    # Render final state
+    GIF = True
+    if GIF:
+        frames = []
+        for t in range(1,env.step_count+1):
+            image = imageio.v2.imread(f'./renderings/step_{t}.png')
+            frames.append(image)
+        imageio.mimsave('./renderings/dqn_training_test_2agents_10000_default.gif', # output gif
+                    frames,          # array of input frames
+                    duration = 500,         # optional: frames per second
+                    loop = 1)        # optional: loop enabled - 1 for True; 0 for False
